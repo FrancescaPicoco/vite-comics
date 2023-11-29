e0bbcbc2a913832cc0317e47d97cd3250b9f4af3
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-29 10:45:16 +0100

    header

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Header from './components/Header.vue'
+
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Header />
+  </div>
+  <AppTitle />
+  <vueimg />
+</template>
+
+<style scoped></style>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,79 @@
+<script>
+export default{
+    nome :"Header",
+    data(){
+        return{
+            links : [
+                {
+                    label: "CHARACTERS",
+                    address: "#"
+                },
+                {
+                    label: "COMICS",
+                    address: "#"
+                },
+                {
+                    label: "MOVIES",
+                    address: "#"
+                },
+                {
+                    label: "TV",
+                    address: "#"
+                },
+                {
+                    label: "GAMES",
+                    address: "#"
+                },
+                {
+                    label: "COLLECTIBLES",
+                    address: "#"
+                },
+                {
+                    label: "VIDEOS",
+                    address: "#"
+                },
+                {
+                    label: "FANS",
+                    address: "#"
+                },
+                {
+                    label: "NEWS",
+                    address: "#"
+                },
+                {
+                    label: "SHOP",
+                    address: "#"
+                },
+
+            ],
+            logo : "/img/dc-logo.png" 
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <section>
+        <img :src="this.logo">
+        <nav>
+            <a v-for="link in links" href="link.address"> {{ link.label }}</a>     
+        </nav>
+    </section>
+    
+</template>
+
+<style scoped>
+section{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 10vh;
+    background-color: aliceblue;
+}
+img{
+    width: 60px;
+    height: 60px;
+}
+</style>
